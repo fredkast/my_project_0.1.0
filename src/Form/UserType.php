4f144c32eeca7fbeee3838358d8924520271c8c9
Frederic Castel <fredericcastel@MacBook-Pro-de-Frederic.local>
2021-05-04 09:16:17 +0200

    Front development

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -16,17 +16,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        // a ameliorer : cacher au utilisateurs mais afficher a l'admin
-            // ->add('ROLES', ChoiceType::class, [
-            //     'choices' => [
-            //         'PARTICULIER' => 'ROLE_USER',
-            //         'PROFESSIONNEL' => 'ROLE_CLIENT',
-            //         'ADMIN' => 'ROLE_ADMIN'
-            //     ],
-            //     'expanded' => true,
-            //     'multiple' => true,
-            //     'label' => 'Rôles' 
-            // ])
+       
             ->add('nom')
             ->add('prenom')
             ->add('adresse')
